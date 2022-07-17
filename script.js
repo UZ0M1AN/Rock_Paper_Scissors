1c55a9779acf12a3b27d34611eb296d137ad78bc
Uzoma S. Uzoma <usumg1165@gmail.com>
2022-07-17 16:36:15 +0100

    Play a round and display the results

--- script.js
+++ script.js
@@ -8,40 +8,52 @@ function computerPlay() {
     return ["rock", "paper", "scissors"][Math.round(Math.random() * 2)];
 }
 
-function playRound(playerSelection, computerSelection) {
-    const rock = 'Rock breaks Scissors',
-          paper = 'Paper covers Rock',
-          scissors = 'Scissors cuts Paper'
+function playRound(e, playerSelection, computerSelection) {
+    playerSelection = e.target.dataset.rps;
+    computerSelection = computerPlay();
+    // console.log(computerSelection, playerSelection);
 
-    const winText = 'You Win!',
-          loseText = 'You Lose!'
+    // if (robotScore == 5 || playerScore == 5) {
+    //     if (robotScore == playerScore) updateDOM("It's a tie!", resultDOM);
+    //     if (robotScore > playerScore) updateDOM("Robot wins!", resultDOM);
+    //     updateDOM("Player wins", resultDOM);
+    // } 
 
+    if (computerSelection === playerSelection) {
+        updateDOM(++robotScore, robotScoreDOM);
+        updateDOM(++playerScore, playerScoreDOM);
+        return;
+    }
     if (computerSelection === 'rock') {
-        if (computerSelection === playerSelection) return 'It\'s a tie!';
-        return playerSelection.toLowerCase() === 'scissors' ? `${loseText} ${rock}` : `${winText} ${paper}`;
+        return playerSelection === 'paper' ? updateDOM(++playerScore, playerScoreDOM) : updateDOM(++robotScore, robotScoreDOM);
     }
     if (computerSelection === 'paper') {
-        if (computerSelection === playerSelection) return 'It\'s a tie!';
-        return playerSelection.toLowerCase() === 'rock' ? `${loseText} ${paper}` : `${winText} ${scissors}`;
+        return playerSelection === 'scissors' ? updateDOM(++playerScore, playerScoreDOM) : updateDOM(++robotScore, robotScoreDOM);
     }
     if (computerSelection === 'scissors') {
-        if (computerSelection === playerSelection) return 'It\'s a tie!';
-        return playerSelection.toLowerCase() === 'paper' ? `${loseText} ${scissors}` : `${winText} ${rock}`;
+        return playerSelection === 'rock' ? updateDOM(++playerScore, playerScoreDOM) : updateDOM(++robotScore, robotScoreDOM);
     }
 
 }
 
-function game(n) {
-    let playerSelection;
-    for (let i = 1; i <= +n; i++) {
-        playerSelection = prompt(`${i}: Rock Paper Scissors`);
-        console.log(i + ': ' + playRound(playerSelection, computerPlay()));
-    }
+function updateDOM (content, contentDOM) {
+    contentDOM.textContent = content;
 }
+
+// function winner (robot, player) {
+//     if (robot === 5) return `${robot}`
+// }
+
+// function game(n) {
+//     let playerSelection;
+//     for (let i = 1; i <= +n; i++) {
+//         playerSelection = prompt(`${i}: Rock Paper Scissors`);
+//         console.log(i + ': ' + playRound(playerSelection, computerPlay()));
+//     }
+// }
 
 //////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
-
 // let startGame;
 
 // do {
@@ -49,3 +61,20 @@ function game(n) {
 //     game(startGame);
 // }
 // while (isNaN(startGame))
+
+//////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+const btns = document.querySelectorAll('.btn');
+const robotScoreDOM = document.querySelector('.robot-score');
+const playerScoreDOM = document.querySelector('.player-score');
+const resultDOM = document.querySelector('.result');
+
+let playerScore = 0;
+let robotScore = 0;
+
+updateDOM(robotScore, robotScoreDOM);
+updateDOM(playerScore, playerScoreDOM);
+
+btns.forEach(btn => btn.addEventListener('click', playRound));
+
+
